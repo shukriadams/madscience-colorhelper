c769c21bed680b0d2409437c7086f23ca9fac321
Shukri Adams <shukri.adams@gmail.com>
2022-01-26 10:29:49 +0100

    refactor

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function lerp (a, b, u) {
-    return Math.floor( (1 - u) * a + u * b)
+    return Math.floor((1 - u) * a + u * b)
 }
 
-function getColorForPercentage (pct, startColor, endColor) {
-    return color = {
-        r: lerp(startColor.r, endColor.r, pct),
-        g: lerp(startColor.g, endColor.g, pct),
-        b: lerp(startColor.b, endColor.b, pct)
+/**
+ * 
+ */
+function getRGBForFactor (factor, startColor, endColor) {
+    return {
+        r: lerp(startColor.r, endColor.r, factor),
+        g: lerp(startColor.g, endColor.g, factor),
+        b: lerp(startColor.b, endColor.b, factor)
     } 
 }
 
@@ -15,25 +18,35 @@ function componentToHex(c) {
     return hex.length == 1 ? '0' + hex : hex
 }
 
-function rgbToHex(rgb) {
-    return `#${componentToHex(rgb.r)}${componentToHex(rgb.g)}${componentToHex(rgb.b)}`
-}
+
 
 module.exports = {
 
+   /**
+    * Converts RGB object to hex.
+    */
+    RGBToHex(rgb = { r : 0, g : 0, b : 0 }) {
+        return `#${componentToHex(rgb.r)}${componentToHex(rgb.g)}${componentToHex(rgb.b)}`
+    },
+
+
     /** 
+     * Determines the  midpoint between two RGB colors based on a factor between 0 and 1.
+     *
      * from https://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage/7128796
      */
-    percentToHex (percent, startColor = {r : 0, g : 250, b : 0 }, endColor = { r : 250, g : 0, b : 0 }) {
-        if (percent < 0)
-            percent = 0
+    factorToRGB (factor, startColor = { r : 0, g : 250, b : 0 }, endColor = { r : 250, g : 0, b : 0 }) {
+        if (factor < 0)
+            factor = 0
 
-        if (percent > 100)
-            percent = 100
+        if (factor > 1)
+            factor = 1
         
-        const rgb = getColorForPercentage(percent, startColor, endColor)
-        return rgbToHex(rgb)
+        return getRGBForFactor(factor, startColor, endColor)
+    },
     
+    factorToHex(factor, startColor = { r : 0, g : 250, b : 0 }, endColor = { r : 250, g : 0, b : 0 }){
+        const rgb = this.factorToRGB(factor, startColor, endColor)
+        return this.RGBToHex(rgb)
     }
-    
 }
